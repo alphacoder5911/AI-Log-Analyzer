ed7c59ebfcc3f00cbfd7b7e5bed683a91427df3d
Shreyas Lalge <lalgeshreyas@gmail.com>
2026-06-11 21:06:34 +0530

    Added insident persistance retrieval and analysis part

--- internal/service/incident_service.go
+++ internal/service/incident_service.go
@@ -1,7 +1,39 @@
 package service
 
-import repository "og_Analyzer/internal/Repository"
+import (
+	"errors"
+	repository "og_Analyzer/internal/Repository"
+	"og_Analyzer/internal/models"
+)
+
+
+type InciServiceInt interface{
+	GetRecentIncidents(limit int) (*[]models.Incidents,error)
+}
 
 type INCI_SERVICE struct {
 	IncidentRepo repository.Incii
+}
+
+
+func NewIncidentService(Repo repository.Incii) *INCI_SERVICE{
+	return &INCI_SERVICE{
+		IncidentRepo: Repo,
+	}
+}
+
+
+func (IS *INCI_SERVICE) GetRecentIncidents(limit int) (*[]models.Incidents,error){
+
+	if limit>20{
+		return nil,errors.New("Limit must not cross 20")
+	}
+
+	res,err:=IS.IncidentRepo.GetRecentIncidents(limit)
+	if err!=nil{
+		return nil,err
+
+	}
+
+	return res,nil
 }
